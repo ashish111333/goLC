987b1748ef831149aeb0476d81e6f73cf6a6513e
ashish111333 <ashishkumar0265aaa@gmail.com>
2026-03-03 01:45:38 +0530

    solved stock1
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- dp/stock_1.go
+++ dp/stock_1.go
@@ -1,1 +1,20 @@
 package dp
+
+func maxProfit(prices []int) int {
+
+	start := 0
+	max_profit := 0
+
+	for i, _ := range prices {
+		if prices[i] > prices[start] {
+			if prices[i]-prices[start] > max_profit {
+				max_profit = prices[i] - prices[start]
+			}
+		}
+		if prices[i] < prices[start] {
+			start = i
+		}
+	}
+	return max_profit
+
+}
